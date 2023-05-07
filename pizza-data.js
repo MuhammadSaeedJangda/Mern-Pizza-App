7f893f990d2c78d39078c998907fa87bcfdc45e5
Muhammad Saeed Jangda <98708732+MuhammadSaeedJangda@users.noreply.github.com>
2023-05-07 12:28:42 +0500

    Add files via upload

---
+++ pizza-data.js
@@ -0,0 +1,93 @@
+const pizzas=[
+  {
+    name: "CHICKEN TIKKA",
+    varients: ["Small", "Medium", "Large"],
+    prices: [
+      {
+        Small: 600,
+        Medium: 1200,
+        Large: 1700,
+      },
+    ],
+    category: "Non Veg",
+    image: "https://assets.indolj.io/images/1677915809-Chicken%20Tikka.jpg",
+    description:
+      "Made with Red Sauce,Chicken Tikka , Onion and Double Layer Cheese",
+  },
+  {
+    name: "CHICKEN FAJITA",
+    varients: ["Small", "Medium", "Large"],
+    prices: [
+      {
+        Small: 600,
+        Medium: 1200,
+        Large: 1700,
+      },
+    ],
+    category: "Non Veg",
+    image: "https://assets.indolj.io/images/1677915797-Chicken%20fajita.jpg",
+    description:
+      "Made with Red Sauce, Chicken Fajita , Onion ,Capsicum and Double Layer Cheese",
+  },
+  {
+    name: "SEEKH KABAB",
+    varients: ["Small", "Medium", "Large"],
+    prices: [
+      {
+        Small: 600,
+        Medium: 1200,
+        Large: 1700,
+      },
+    ],
+    category: "Non Veg",
+    image: "https://assets.indolj.io/images/1677915540-Seekh%20kabab.jpg",
+    description:
+      "Made with Creamy Chaska sauce, Seekh kebab , Onion & Double Layer Cheese",
+  },
+  {
+    name: "CHEESE LOVER",
+    varients: ["Small", "Medium", "Large"],
+    prices: [
+      {
+        Small: 600,
+        Medium: 1200,
+        Large: 1700,
+      },
+    ],
+    category: "Non Veg",
+    image: "https://assets.indolj.io/images/1677915528-Cheese%20lover.jpg",
+    description:
+      "Made with Red Sauce, Marvelous heavy cheese topped with Mozzarella Cheese",
+  },
+  {
+    name: "RANCH",
+    varients: ["Small", "Medium", "Large"],
+    prices: [
+      {
+        Small: 600,
+        Medium: 1200,
+        Large: 1700,
+      },
+    ],
+    category: "Non Veg",
+    image: "https://assets.indolj.io/images/1677915240-Ranch.jpg",
+    description:
+      "Ranch Sauce with Chicken Fajita,Onions,Mushroom Red & Green Jalapeno, black olives & double Cheese with drizzile with Ranch Sauce",
+  },
+  {
+    name: "CHIPOTLE",
+    varients: ["Small", "Medium", "Large"],
+    prices: [
+      {
+        Small: 600,
+        Medium: 1200,
+        Large: 1700,
+      },
+    ],
+    category: "Non Veg",
+    image: "https://assets.indolj.io/images/1677915227-Chipotle.jpg",
+    description:
+      "Made with Chipotle Sauce topped with jamaican chicken , Red Chillies ,Green Pepper , Olive Onion & double Cheese() drizzile with Chipotle Sauce",
+  },
+];
+module.exports= pizzas;
